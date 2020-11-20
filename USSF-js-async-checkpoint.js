5e5bf5fec664c625d30f4f0ccb706aeab66c8967
m-harp <starmarine2093@gmail.com>
2020-11-20 08:07:47 -0600

    update
    update

--- USSF-js-async-checkpoint.js
+++ USSF-js-async-checkpoint.js
@@ -1,11 +1,13 @@
 var fs = require('fs')
-var fetch = require('fetch')
+var fetch = require('node-fetch')
 
 let pokemon = 'butterfree'
 
 function fetchpokemon() {
 fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 .then(response => response.json())
-.then(json => (json.type.map(element => element.type.name)) 
-.then(data => console.log(pokemon + ": " + json.join(", ")))
+.then(json => (json.types.map(element => element.type.name)))
+.then(data => console.log(pokemon + ": " + data.join(", ")))
 }
+
+fetchpokemon();
